af25c95a192699dba8182d5280bbeb4e1c2e7e0e
JavierZavala17 <javier_z_t@hotmail.com>
2025-12-14 20:38:17 -0600

    use sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_characters, dict_to_sorted_list 
+import sys
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,19 +7,22 @@ def get_book_text(path_to_file):
         return file_content
 
 def main():
-    relative_path_to_frankenstein = "./books/frankenstein.txt"
-    frankenstein = get_book_text(relative_path_to_frankenstein)
-    
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(get_num_words(frankenstein))
-    print("--------- Character Count -------")
-    dict_char_count = get_num_characters(frankenstein)
-    sorted_list = dict_to_sorted_list(dict_char_count)
-    for d in sorted_list:
-        print(f"{d['char']}: {d['num']}")
-    print("============= END ===============")
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:   
+        book = get_book_text(sys.argv[1])
+        
+        print("============ BOOKBOT ============")
+        print("Analyzing book found at books/frankenstein.txt...")
+        print("----------- Word Count ----------")
+        print(get_num_words(book))
+        print("--------- Character Count -------")
+        dict_char_count = get_num_characters(book)
+        sorted_list = dict_to_sorted_list(dict_char_count)
+        for d in sorted_list:
+            print(f"{d['char']}: {d['num']}")
+        print("============= END ===============")
 
 
 main()
